83381d34ea9501f0f7932c64e14da4a304e83ac9
stefanjohne1-dotcom <stefanjohne1@gmail.com>
2026-02-14 20:21:21 +0100

    Update BlockCode.py

--- BlockCode.py
+++ BlockCode.py
@@ -123,8 +123,8 @@ elif st.session_state.page == 'scanner':
 
 # --- SAMMLUNG ---
 elif st.session_state.page == 'sammlung':
-if st.button("⬅️ ZURÜCK"): st.session_state.page = 'home'; st.rerun()
-   st.title("📚 Deine Sammlung")
+    if st.button("⬅️ ZURÜCK"): st.session_state.page = 'home'; st.rerun()
+       st.title("📚 Deine Sammlung")
     
        p = get_live_prices()
     try:
@@ -175,5 +175,6 @@ if st.button("⬅️ ZURÜCK"): st.session_state.page = 'home'; st.rerun()
         else:
 st.info("Noch keine Münzen gespeichert.")
     except Exception as e: st.error(f"Fehler: {e}")
+
 
 
