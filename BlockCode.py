83ca85e23b8d9d7a5a3239fbf92a91608f04176d
stefanjohne1-dotcom <stefanjohne1@gmail.com>
2026-02-17 09:46:47 +0100

    Update BlockCode.py

--- BlockCode.py
+++ BlockCode.py
@@ -499,10 +499,11 @@ elif st.session_state.page == 'sammlung':
 
         else:
 
-st.info("Das Archiv ist aktuell noch leer. Scanne deine erste Münze!")
+        st.info("Das Archiv ist aktuell noch leer. Scanne deine erste Münze!")
 
             
 
     except Exception as e:
 
         st.error(f"Fehler beim Laden: {e}")
+
